4b647ba00075d53fa8d281c41cf9957d87880ebe
Tú Nguyễn <7784783-CamTuJV-IT@users.noreply.gitlab.com>
2021-04-17 19:30:22 +0700

    style: color banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="v3-banner">
     <span class="hidden-sm"
-      >If you like WebdriverIO, give it a star on</span
+      >If you like Future Brand Việt Nam, give it a star on</span
     >
     <a href="https://v3.vuejs.org/">Click here</a> !
   </div>
@@ -15,12 +15,16 @@ export default {};
   #v3-banner {
     display: block;
     // background-color: #42b983;
-    background-color: #32a403;
+    background-color: #71e271;
     min-height: 40px;
     padding: 10px 60px;
     z-index: 19;
     box-sizing: border-box;
     text-align: center;
     color: #d6ffec;
+    a {
+      color: #fff;
+      font-weight: bold;
+    }
   }
 </style>
